965a1851932b34b6749aa6d58c68c44773c902d3
panicoenlaxbox <panicoenlaxbox@gmail.com>
2016-07-12 17:04:24 +0200

    Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Singleton
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Singleton.Instance().Foo();
+		}
+	}
+
+	class Singleton
+	{
+		private static Singleton _instance;
+		private Singleton()
+		{
+
+		}
+		public static Singleton Instance()
+		{
+			return _instance ?? (_instance = new Singleton());
+		}
+		public void Foo()
+		{
+			Console.WriteLine("Foo");
+		}
+	}
+}
