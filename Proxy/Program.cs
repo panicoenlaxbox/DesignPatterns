9f1ad9bbe1a1f27a17b6f85e2df59a26c1eba106
panicoenlaxbox <panicoenlaxbox@gmail.com>
2016-07-16 13:12:42 +0200

    Proxy

---
+++ Proxy/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Proxy
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Foo(new RealSubject());
+			Foo(new Proxy());
+		}
+
+		public static void Foo(Subject subject)
+		{
+			subject.Operation();
+		}
+	}
+
+	interface Subject
+	{
+		void Operation();
+	}
+
+	class RealSubject : Subject
+	{
+		public void Operation()
+		{
+			Console.WriteLine("Operation in RealSubject");
+		}
+	}
+
+	class Proxy : Subject
+	{
+		private Subject _realSubject;
+		public void Operation()
+		{
+			Console.WriteLine("Operation in proxy");
+			if (_realSubject == null) {
+				_realSubject = new RealSubject();
+			}
+			_realSubject.Operation();
+		}
+	}
+}
