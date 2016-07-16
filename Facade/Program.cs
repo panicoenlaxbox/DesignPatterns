49685b5bbc13c899995d8479cb611cdcb7776088
panicoenlaxbox <panicoenlaxbox@gmail.com>
2016-07-16 12:38:40 +0200

    Facade

---
+++ Facade/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Facade
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+
+	class ComplexClass1
+	{
+		public void ComplexOperation1()
+		{
+		}
+	}
+
+	class ComplexClass2
+	{
+		public void ComplexOperation2()
+		{
+		}
+	}
+
+	class Facade
+	{
+		readonly ComplexClass1 _complexClass1;
+		readonly ComplexClass2 _complexClass2;
+		public Facade()
+		{
+			_complexClass1 = new ComplexClass1();
+			_complexClass2 = new ComplexClass2();
+		}
+		public void Operation()
+		{
+			_complexClass1.ComplexOperation1();
+			_complexClass2.ComplexOperation2();
+		}
+	}
+}
