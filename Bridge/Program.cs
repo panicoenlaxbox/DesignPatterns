52e945203f1132f929f926b4bd5b6a83d255431c
panicoenlaxbox <panicoenlaxbox@gmail.com>
2016-07-18 01:54:01 +0200

    Bridge

---
+++ Bridge/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Bridge
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var concreteAbstraction = new ConcreteAbstraction(new ConcreteImplementor());
+			concreteAbstraction.Operation();
+		}
+	}
+
+	abstract class Abstraction
+	{
+		protected readonly Implementor implementor;
+
+		protected Abstraction(Implementor implementor)
+		{
+			this.implementor = implementor;
+		}
+		public virtual void Operation()
+		{
+			this.implementor.Implementation();
+		}
+	}
+
+	class ConcreteAbstraction : Abstraction
+	{
+		public ConcreteAbstraction(Implementor implementor) : base(implementor)
+		{
+		}
+	}
+
+	interface Implementor
+	{
+		void Implementation();
+	}
+
+	class ConcreteImplementor : Implementor
+	{
+		public void Implementation()
+		{
+			Console.WriteLine("ConcreteImplementor.Implementation");
+		}
+	}
+}
