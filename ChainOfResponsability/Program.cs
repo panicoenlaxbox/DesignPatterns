48ff6344af81e41c94d20b48608f4cfbf1f95347
panicoenlaxbox <panicoenlaxbox@gmail.com>
2016-07-18 21:42:55 +0200

    ChainOfResponsability

---
+++ ChainOfResponsability/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ChainOfResponsability
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var a = new ConcreteHandler1();
+			var b = new ConcreteHandler2();
+			a.NextHandler = b;
+			Console.WriteLine(a.HandleRequest());
+		}
+
+		abstract class Handler
+		{
+
+			protected abstract string Name { get; }
+
+			public virtual string HandleRequest()
+			{
+				string name = Name;
+				if (name != null)
+					return name;
+				if (NextHandler != null)
+					return NextHandler.HandleRequest();
+				return "default name";
+
+			}
+			public Handler NextHandler { get; set; }
+		}
+
+		class ConcreteHandler1 : Handler
+		{
+			protected override string Name
+			{
+				get
+				{
+					return null;
+				}
+			}
+
+		}
+
+		class ConcreteHandler2 : Handler
+		{
+			protected override string Name
+			{
+				get
+				{
+					return "ConcreteHandler2";
+				}
+			}
+
+		}
+	}
+}
