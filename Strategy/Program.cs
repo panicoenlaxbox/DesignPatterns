c406a8bb57278a1975d0425815d9c0f84f1cd7a7
panicoenlaxbox <panicoenlaxbox@gmail.com>
2016-07-18 16:11:46 +0200

    Strategy

---
+++ Strategy/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Strategy
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var entidad = new Entidad(new EstrategiaConcretaA());
+			entidad.Operacion();
+			entidad = new Entidad(new EstrategiaConcretaB());
+			entidad.Operacion();
+		}
+	}
+
+	class Entidad
+	{
+		readonly Estrategia estrategia;
+
+		public Entidad(Estrategia estrategia)
+		{
+			this.estrategia = estrategia;
+		}
+		public void Operacion()
+		{
+			estrategia.Calcula();
+		}
+	}
+
+	interface Estrategia
+	{
+		void Calcula();
+	}
+
+	class EstrategiaConcretaA : Estrategia
+	{
+		public void Calcula()
+		{
+			Console.WriteLine("EstrategiaConcretaA");
+		}
+	}
+
+	class EstrategiaConcretaB : Estrategia
+	{
+		public void Calcula()
+		{
+			Console.WriteLine("EstrategiaConcretaB");
+		}
+	}
+}
