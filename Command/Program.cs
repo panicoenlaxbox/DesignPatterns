019ec7e7b7875263b7537f928e2356372ed35bc6
panicoenlaxbox <panicoenlaxbox@gmail.com>
2016-07-20 18:02:53 +0200

    Command

---
+++ Command/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Command
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Receiver receiver = new Receiver();
+			Command command = new ConcreteCommand(receiver);
+			Invoker invoker = new Invoker();
+			invoker.SetCommand(command);
+			invoker.ExecuteCommand();
+		}
+	}
+
+	abstract class Command
+	{
+		protected readonly Receiver receiver;
+
+		public Command(Receiver receiver)
+		{
+			this.receiver = receiver;
+		}
+
+		public abstract void Execute();
+	}
+
+	class ConcreteCommand : Command
+	{
+		public ConcreteCommand(Receiver receiver) : base(receiver)
+		{
+
+		}
+		public override void Execute()
+		{
+			receiver.Action();
+		}
+	}
+
+	class Receiver
+	{
+		public void Action()
+		{
+			Console.WriteLine("Receiver.Action");
+		}
+	}
+
+	class Invoker
+	{
+		private Command command;
+
+		public void SetCommand(Command command)
+		{
+			this.command = command;
+		}
+
+		public void ExecuteCommand()
+		{
+			this.command.Execute();
+		}
+	}
+}
