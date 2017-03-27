97d157ec2ba5c7f76831d75980c1635dd3115d9c
panicoenlaxbox <panicoenlaxbox@gmail.com>
2017-03-27 09:38:25 +0200

    Update Program.cs

--- Observer/Program.cs
+++ Observer/Program.cs
@@ -1,80 +1,84 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Observer
 {
-	class MainClass
-	{
-		public static void Main(string[] args)
-		{
-			var subject = new ConcreteSubject();
-			var observer1 = new ConcreteObserver(subject);
-			var observer2 = new ConcreteObserver(subject);
-			subject.Name = "panicoenlaxbox";
-		}
-	}
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var subject = new ConcreteSubject();
+            var observer1 = new ConcreteObserver(subject);
+            var observer2 = new ConcreteObserver(subject);
+            subject.Name = "panicoenlaxbox";
+        }
+    }
 
-	abstract class Subject
-	{
-		readonly List<Observer> observers;
-		public Subject()
-		{
-			observers = new List<Observer>();
-		}
-		public void Attach(Observer observer)
-		{
-			observers.Add(observer);
-		}
-		public void Detach(Observer observer)
-		{
-			observers.Remove(observer);
-		}
-		protected void Notify()
-		{
-			foreach (var o in observers)
-			{
-				o.Update();
-			}
-		}
-	}
+    internal abstract class Subject
+    {
+        private readonly List<Observer> _observers;
 
-	interface Observer
-	{
-		void Update();
-	}
+        protected Subject()
+        {
+            _observers = new List<Observer>();
+        }
 
-	class ConcreteSubject : Subject
-	{
-		string _name;
+        public void Attach(Observer observer)
+        {
+            _observers.Add(observer);
+        }
 
-		public string Name
-		{
-			get
-			{
-				return _name;
-			}
-			set
-			{
-				_name = value;
-				Notify();
-			}
-		}
-	}
+        public void Detach(Observer observer)
+        {
+            _observers.Remove(observer);
+        }
 
-	class ConcreteObserver : Observer
-	{
-		readonly ConcreteSubject subjet;
+        protected void Notify()
+        {
+            foreach (var observer in _observers)
+            {
+                observer.Update();
+            }
+        }
+    }
 
-		public ConcreteObserver(ConcreteSubject subjet)
-		{
-			this.subjet = subjet;
-			subjet.Attach(this);
-		}
+    internal interface Observer
+    {
+        void Update();
+    }
 
-		public void Update()
-		{
-			Console.WriteLine("Observer has been notified");
-			Console.WriteLine("New name of subject is " + subjet.Name);
-		}
-	}
+    internal class ConcreteSubject : Subject
+    {
+        private string _name;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                Notify();
+            }
+        }
+    }
+
+    internal class ConcreteObserver : Observer
+    {
+        private readonly ConcreteSubject _subjet;
+
+        public ConcreteObserver(ConcreteSubject subjet)
+        {
+            _subjet = subjet;
+            subjet.Attach(this);
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Observer has been notified");
+            Console.WriteLine("New name of subject is " + _subjet.Name);
+        }
+    }
 }
